7a7691b7b7f527c80ef5ede8dee208b5898fda95
xLalice <dreamsend08@gmail.com>
2023-08-08 02:51:32 +0800

    Add logic to handle audio playback duration and prevent overlapping playback

--- drum-machine/src/Drumpad.jsx
+++ drum-machine/src/Drumpad.jsx
@@ -1,28 +1,33 @@
-import React from "react"
+import React from "react";
 
-export default function Drumpad(props){
-    function playAudio(){
-        const audio = document.getElementById(props.id)
-        audio.play();
-        props.changeDisplay(props.description)
+export default function Drumpad(props) {
+  function playAudio() {
+    const audio = document.getElementById(props.id);
+    // Reset the audio to the beginning if it's still playing
+    if (!audio.paused) {
+      audio.currentTime = 0;
+    }
+    audio.play();
+    props.changeDisplay(props.description);
+  }
+
+  React.useEffect(() => {
+    function handleKeyPress(event) {
+      if (event.key.toUpperCase() === props.id.toUpperCase()) {
+        playAudio();
+      }
     }
 
-    React.useEffect(() => {
-        function handleKeyPress(event) {
-          if (event.key.toUpperCase() === props.id.toUpperCase()) {
-            playAudio();
-          }
-        }
-        window.addEventListener("keydown", handleKeyPress);
-        return () => {
-          window.removeEventListener("keydown", handleKeyPress);
-        };
-    }, [props.id]);
-    
-    return (
-        <div className="drum-pad" onClick={playAudio}>
-            {props.id}
-            <audio id={props.id} class="clip" src={props.src}></audio>
-        </div>
-    )
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [props.id]);
+
+  return (
+    <div className="drum-pad" onClick={playAudio}>
+      {props.id}
+      <audio id={props.id} className="clip" src={props.src}></audio>
+    </div>
+  );
 }
